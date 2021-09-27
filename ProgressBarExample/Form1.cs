bc996f110432fa7506ab96b322cd68ce66ce8d6e
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-27 21:27:50 +0600

    Project has been done...

--- ProgressBarExample/Form1.cs
+++ ProgressBarExample/Form1.cs
@@ -19,14 +19,23 @@ namespace ProgressBarExample
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            // Set a maximum value
             progressBar1.Maximum = 10000;
+
+            //set a minimum value
             progressBar1.Minimum = 0;
+
+            //set step value
             progressBar1.Step = 1;
 
+            //set the style of the progress bar
             progressBar1.Style = ProgressBarStyle.Continuous;
 
+            //create a loop to incrase the value
             for(int i=0; i<progressBar1.Maximum; i++)
             {
+                //set the new value of progress bar
                 progressBar1.Value = i;
             }
         }
