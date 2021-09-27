c3cc6d9942dbf2c345a6986710ecb730b4b1b490
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-27 21:24:35 +0600

    Progress bar has been created..

--- ProgressBarExample/Form1.Designer.cs
+++ ProgressBarExample/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace ProgressBarExample
         /// </summary>
         private void InitializeComponent()
         {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(73, 111);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(495, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(644, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +64,9 @@ namespace ProgressBarExample
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ProgressBarExample/Form1.cs
+++ ProgressBarExample/Form1.cs
@@ -17,5 +17,18 @@ namespace ProgressBarExample
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 10000;
+            progressBar1.Minimum = 0;
+            progressBar1.Step = 1;
+
+            progressBar1.Style = ProgressBarStyle.Continuous;
+
+            for(int i=0; i<progressBar1.Maximum; i++)
+            {
+                progressBar1.Value = i;
+            }
+        }
     }
 }
